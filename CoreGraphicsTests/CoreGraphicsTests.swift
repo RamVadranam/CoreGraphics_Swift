6d102a47f75394c62e73c064896350de7cba4d78
ram-123 <ramchandra.vadranam@gmail.com>
2014-07-04 14:44:48 +0100

    Initial Commit

---
+++ CoreGraphicsTests/CoreGraphicsTests.swift
@@ -0,0 +1,35 @@
+//
+//  CoreGraphicsTests.swift
+//  CoreGraphicsTests
+//
+//  Created by Sriram Srinivasan on 04/07/2014.
+//  Copyright (c) 2014 Amsphere. All rights reserved.
+//
+
+import XCTest
+
+class CoreGraphicsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
